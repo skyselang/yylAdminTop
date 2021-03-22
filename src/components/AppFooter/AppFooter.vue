05ad1e86ab114135e40f837877261bac5598cb13
skyselang <215817969@qq.com>
2021-03-22 14:09:33 +0800

    友情链接：芦虎导航

--- src/components/AppFooter/AppFooter.vue
+++ src/components/AppFooter/AppFooter.vue
@@ -2,6 +2,11 @@
   <div>
     <el-row>
       <el-col :span="24">
+        <div class="copyright">友情链接：<a href="https://www.luhu.co" target="_blank">芦虎导航</a></div>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
         <div class="copyright">Copyright © 2020 yylAdmin, Apache-2.0 license</div>
       </el-col>
     </el-row>
@@ -18,6 +23,10 @@ export default {
 </script>
 
 <style scoped>
+a {
+  text-decoration: none;
+  color:black;
+}
 .copyright {
   text-align: center;
 }
