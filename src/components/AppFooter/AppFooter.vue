a1eccb2761917f8c8a6f24b7ddce0550284235e8
skyselang <215817969@qq.com>
2021-09-22 02:20:18 +0000

    修改：ICP

--- src/components/AppFooter/AppFooter.vue
+++ src/components/AppFooter/AppFooter.vue
@@ -4,13 +4,13 @@
       <el-col :span="24">
         <div class="copyright">
           <a href="https://www.luhu.co" target="_blank">芦虎导航</a>
-          <a class="margin-left" href="https://beian.miit.gov.cn" target="_blank">粤ICP备2020084102号</a>
+          <a class="margin-left" href="https://beian.miit.gov.cn" target="_blank">桂ICP备2021007680号-1</a>
         </div>
       </el-col>
     </el-row>
     <el-row>
       <el-col :span="24">
-        <div class="copyright">Copyright © 2020 yylAdmin, Apache-2.0 license</div>
+        <div class="copyright">Copyright © 2021 yylAdmin, Apache-2.0 license</div>
       </el-col>
     </el-row>
   </div>
